65575aefdd04119fe81349c08dd2c70eac7fd259
Leon Colt <lionmanbloodsucker@yahoo.com>
2024-01-11 12:28:22 +0700

    add unit test write read kv2

--- vaultmanager_test.go
+++ vaultmanager_test.go
@@ -1,11 +1,13 @@
 package vaultmanager_test
 
 import (
+	"errors"
 	"sync"
 	"testing"
 	"time"
 
 	"github.com/SolusiDataPintar/vaultmanager"
+	"github.com/hashicorp/vault/api"
 	"github.com/stretchr/testify/require"
 	"golang.org/x/net/context"
 )
@@ -21,6 +23,57 @@ func TestGetClient(t *testing.T) {
 
 	c := vaultmanager.GetClient()
 	require.NotNil(t, c)
+}
+
+func TestWriteKVv2(t *testing.T) {
+	err := vaultmanager.Open(nil)
+	require.NoError(t, err)
+
+	data := map[string]any{
+		"test1": "test5",
+		"test2": "test4",
+		"test3": "test3",
+		"test4": "test2",
+		"test5": "test1",
+	}
+	err = vaultmanager.WriteKVv2(context.Background(), "chainsmart", "test/vault-manager-write", data)
+	require.NoError(t, err)
+
+	err = vaultmanager.GetClient().KVv2("chainsmart").Delete(context.Background(), "test/vault-manager-write")
+	require.NoError(t, err)
+}
+
+func TestReadKVv2(t *testing.T) {
+	err := vaultmanager.Open(nil)
+	require.NoError(t, err)
+
+	data := map[string]any{
+		"test1": "test5",
+		"test2": "test4",
+		"test3": "test3",
+		"test4": "test2",
+		"test5": "test1",
+	}
+	err = vaultmanager.WriteKVv2(context.Background(), "chainsmart", "test/vault-manager-read", data)
+	require.NoError(t, err)
+
+	secretData, err := vaultmanager.ReadKVv2(context.Background(), "chainsmart", "test/vault-manager-read")
+	require.NoError(t, err)
+	require.Equal(t, data, secretData)
+
+	err = vaultmanager.GetClient().KVv2("chainsmart").Delete(context.Background(), "test/vault-manager-read")
+	require.NoError(t, err)
+}
+
+func TestReadKVv2NotFound(t *testing.T) {
+	err := vaultmanager.Open(nil)
+	require.NoError(t, err)
+
+	secretData, err := vaultmanager.ReadKVv2(context.Background(), "chainsmart", "test/vault-manager-not-found")
+	require.EqualError(t, err, api.ErrSecretNotFound.Error())
+	require.True(t, errors.Is(err, api.ErrSecretNotFound))
+	require.NotNil(t, secretData)
+	require.Empty(t, secretData)
 }
 
 func TestManageTokenLifecycle(t *testing.T) {
